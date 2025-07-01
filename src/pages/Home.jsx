e3be5c7e4d7f663f8921b6d2aba945ea3993261d
ycho60@my.bcit.ca <ycho60@my.bcit.ca>
2025-06-30 21:08:24 -0700

    just a bit of change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ function Home() {
                     <div className="col">
                         <div className="blur-2">
                             <h1 className="hero-title">
-                                Heyy <br /> I'm{" "} <br />
+                                Heyy<br /> I'm{" "} <br />
                                 <span>YunGi Cho</span>
                                 <br /> UI/UX Designer
                             </h1>
