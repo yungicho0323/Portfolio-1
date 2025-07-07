007b5b0074af963363be325d3ac2286fa2018c0e
ycho60@my.bcit.ca <ycho60@my.bcit.ca>
2025-07-07 09:54:56 -0700

    Card.jsx and Card.css
    make the images clickable and linked to project detail pages

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,111 +1,113 @@
 import { Card, CardHeader, CardBody, CardFooter, Typography, Button } from "@material-tailwind/react";
-// import ZynergyImage from './assets/images/Zynergy.png';
 import './Card.css';
 import "animate.css";
 import ZynergyImage from "../assets/images/iphone-16-mockup.png";
 import FurryTaleImage from "../assets/images/FurryTales-laptop.png";
 import MedSpeakImage from "../assets/images/MedSpeak.png";
-import { Link }from 'react-router';
+import { Link } from 'react-router';
 import babelImage from '../assets/images/BabelMockup.png';
-
-
 
 export function CardComponent() {
   return (
     <Card className="card-container">
 
-      <div class="image-wrapper animate__animated animate__slideInUp">
-        <img src={MedSpeakImage} alt="Project-3" className="project-image3" />
-      {/*MedSpeak Title*/}
+      {/* MedSpeak Card - Entire card is clickable */}
+      <Link to="/MedSpeak" className="card-link">
+        <div className="image-wrapper animate__animated animate__slideInUp">
+          <img src={MedSpeakImage} alt="Project-3" className="project-image3" />
+          {/*MedSpeak Title*/}
           <Typography variant="h5" color="blue-gray" className="MedSpeak"> 
-          MedSpeak: AI-powered Medical Translation App
+            MedSpeak: AI-powered Medical Translation App
           </Typography>
 
-      {/*Card Bottom Section */}
-        <div className="bottom-section">
-          <div className="left-section">
-        <CardFooter className="cardFooter-wrapper">
-          <Link to="/MedSpeak" className="card-button">Read More</Link>
-        </CardFooter>
-        </div>
+          {/*Card Bottom Section */}
+          <div className="bottom-section">
+            <div className="left-section">
+              <CardFooter className="cardFooter-wrapper">
+                <span className="card-button">Read More</span>
+              </CardFooter>
+            </div>
 
-        <div className="project-type-badge">
-        <button className="project-type-button">UI/ UX</button>
-        </div> 
+            <div className="project-type-badge">
+              <button className="project-type-button">UI/ UX</button>
+            </div> 
+          </div>
         </div>
-      </div>
+      </Link>
 
-      <div class="image-wrapper animate__animated animate__slideInUp">
-        <img src={babelImage} alt="Project-4" className="project-image4" />
-      {/*2 Seconds Title*/}
+      {/* Babel Card - Entire card is clickable */}
+      <Link to="/babel" className="card-link">
+        <div className="image-wrapper animate__animated animate__slideInUp">
+          <img src={babelImage} alt="Project-4" className="project-image4" />
+          {/*Babel Title*/}
           <Typography variant="h5" color="blue-gray" className="babel"> 
-          Babel: Safety-driven, Real-time Location Sharing App
+            Babel: Safety-driven, Real-time Location Sharing App
           </Typography>
 
-      {/*Card Bottom Section */}
-        <div className="bottom-section">
-          <div className="left-section">
-        <CardFooter className="cardFooter-wrapper">
-          <Link to="/babel" className="card-button">Read More</Link>
-        </CardFooter>
-        </div>
+          {/*Card Bottom Section */}
+          <div className="bottom-section">
+            <div className="left-section">
+              <CardFooter className="cardFooter-wrapper">
+                <span className="card-button">Read More</span>
+              </CardFooter>
+            </div>
 
-        <div className="project-type-badge">
-        <button className="project-type-button">UI/ UX</button>
-        </div> 
+            <div className="project-type-badge">
+              <button className="project-type-button">UI/ UX</button>
+            </div> 
+          </div>
         </div>
-      </div>
+      </Link>
 
-    <div class="image-wrapper animate__animated animate__slideInUp">
-        <img src={FurryTaleImage} alt="Project-2" className="project-image2" />
-      {/*FurryTales Title*/}
+      {/* FurryTales Card - Entire card is clickable */}
+      <Link to="/furrytales" className="card-link">
+        <div className="image-wrapper animate__animated animate__slideInUp">
+          <img src={FurryTaleImage} alt="Project-2" className="project-image2" />
+          {/*FurryTales Title*/}
           <Typography variant="h5" color="blue-gray" className="furryTales"> 
-           FurryTales Pet: 
+            FurryTales Pet: 
             FLUI Hackathon 2025
           </Typography>
 
-      {/*Card Bottom Section */}
-        <div className="bottom-section">
-          <div className="left-section">
-        <CardFooter className="cardFooter-wrapper">
+          {/*Card Bottom Section */}
+          <div className="bottom-section">
+            <div className="left-section">
+              <CardFooter className="cardFooter-wrapper">
+                <span className="card-button">Read More</span>
+              </CardFooter>
+            </div>
 
-          <Link to="/furrytales" className="card-button">Read More
-          </Link>
-        </CardFooter>
+            <div className="project-type-badge">
+              <button className="project-type-button">Website Redesign</button>
+            </div>
+          </div>
         </div>
+      </Link>
 
-        <div className="project-type-badge">
-        <button className="project-type-button">Website Redesign</button>
-        </div>
-        </div>
-      </div>
-
-    <div class="image-wrapper animate__animated animate__slideInUp">
-        <img src={ZynergyImage} alt="Project-1" className="project-image1" />
-        {/*Znergy Title*/}
+      {/* Zynergy Card - Entire card is clickable */}
+      <Link to="/zynergy" className="card-link">
+        <div className="image-wrapper animate__animated animate__slideInUp">
+          <img src={ZynergyImage} alt="Project-1" className="project-image1" />
+          {/*Zynergy Title*/}
           <Typography variant="h5" color="blue-gray" className="zynergy">
             Zynergy: Group Workout Challenge Fitness App
           </Typography>
-        {/*Card Bottom Section*/}
-        <div className="bottom-section">
-          <div className="left-section">
-        <CardFooter className="cardFooter-wrapper">
-          <Link to="/zynergy" className="card-button">Read More</Link>
-        </CardFooter>
+          {/*Card Bottom Section*/}
+          <div className="bottom-section">
+            <div className="left-section">
+              <CardFooter className="cardFooter-wrapper">
+                <span className="card-button">Read More</span>
+              </CardFooter>
+            </div>
+            
+            <div className="project-type-badge">
+              <button className="project-type-button">UI / UX</button>
+            </div>
+          </div>
         </div>
-        
-        <div className="project-type-badge">
-        <button className="project-type-button">UI / UX</button>
-        </div>
-        </div>
-    </div>
-
-    
-
-
+      </Link>
 
     </Card>
-    
   );
 }
 
