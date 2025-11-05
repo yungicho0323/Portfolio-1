8545cdd95af6d60d15d56099b77b9b8b89379e12
ycho60@my.bcit.ca <ycho60@my.bcit.ca>
2025-11-04 16:06:19 -0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
         {/* HERO (reference-style structure) */}
         <section className="hero">
         <h1 className="hero-title">
-            Transforming complex challenges into <br />
+            Transforming daily challenges into <br />
             <span className="brand-highlight big-highlight">
             inclusive, data-informed experiences
             </span>
