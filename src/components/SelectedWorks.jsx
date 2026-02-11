8f95e16adc9ce8739194a657285c73bc3eecb547
ycho60@my.bcit.ca <ycho60@my.bcit.ca>
2026-02-10 22:20:35 -0800

    Aperture Coffee Bar Case study addition

--- src/components/SelectedWorks.jsx
+++ src/components/SelectedWorks.jsx
@@ -3,6 +3,7 @@ import './Card.css';
 import "animate.css";
 import FurryTaleImage from "../assets/images/FurryTales-laptop.png";
 import BuddyUpPlusImage from "../assets/images/iphone-16-mockup.png";
+import ApertureImage from "../assets/images/aperturemockup.png";
 import MedSpeakImage from "../assets/images/MedSpeak.png";
 import { Link } from 'react-router-dom';
 
@@ -10,6 +11,7 @@ export function CardComponent() {
   return (
     <div className="card-container">
       
+
  {/* BuddyUP+ Card */}
       <Link to="/BuddyUpPlus" className="card-link" aria-label="BuddyUp+ case study">
         <div className="image-wrapper animate__animated animate__slideInUp">
@@ -30,6 +32,28 @@ export function CardComponent() {
           </div>
         </div>
       </Link>
+
+       {/*Aperture Coffee Bar Website Redesign Card */}
+      <Link to="/" className="card-link" aria-label="BuddyUp+ case study">
+        <div className="image-wrapper animate__animated animate__slideInUp">
+          <img src={ApertureImage} alt="BuddyUp+ project cover" className="project-image1" />
+
+          {/* new text block */}
+          <div className="project-text-block">
+            <span className="project-name">Aperture Coffee bar</span>
+
+            <h3 className="project-tagline">Redesigning a local café website to better reflect its brand, events, and in-store experience.</h3>
+          </div>
+
+          <div className="bottom-section">
+            <div className="right-section">
+              <CardFooter className="cardFooter-wrapper">
+                <span className="card-button">It's coming soon!</span>
+              </CardFooter>
+            </div>
+          </div>
+        </div>
+      </Link>
     
           {/* FurryTales Card */}
           <Link to="/furrytales" className="card-link" aria-label="FurryTales case study">
@@ -38,7 +62,7 @@ export function CardComponent() {
     
               {/* new text block */}
               <div className="project-text-block">
-                <span className="project-name">FurryTales Pet</span>
+                <span className="project-name">Hackathon @ Emaily Carr University</span>
                 <h3 className="project-tagline">Redesigning a local pet brand for joyful, seamless shopping.</h3>
               </div>
     
